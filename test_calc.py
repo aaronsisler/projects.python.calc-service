ed81e0e3cf655766bba9b62b2f79dde002702cb7
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-07-17 12:29:06 -0400

    Quick project for learning unittest in Python

---
+++ test_calc.py
@@ -0,0 +1,20 @@
+import unittest
+# import calc
+from calc import add, divide
+
+
+class TestCalc(unittest.TestCase):
+
+    def test_add(self):
+        expectedResult = 15
+        result = add(10, 5)
+        self.assertEqual(expectedResult, result)
+
+    def test_divide(self):
+        # self.assertRaises(ValueError, divide, 10, 0)
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
