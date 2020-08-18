2c86adc318860f8a39e10c0438f48f33f3f79d49
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2020-08-18 16:10:11 -0400

    Holding this noise

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="add-two-integers",
     version="0.1.0",
-    packages=find_packages()
+    packages=find_packages(include=("src"), exclude=("src/local-app")),
+    py_modules=["requests"]
 )
